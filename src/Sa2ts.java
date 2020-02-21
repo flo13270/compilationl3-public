abe5970c2e1a59c2e6bc822fab7c31837e9cf1dc
n19018305 <n19018305@L-054011F110-17.salsa.univ-amu.fr>
2020-02-21 17:50:29 +0100

    j'ai fais des choses

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -3,62 +3,87 @@ import sa.SaDecFonc;
 import sa.SaDecTab;
 import sa.SaDecVar;
 import sa.SaDepthFirstVisitor;
-import sa.SaInstRetour;
 import sa.SaNode;
 import sa.SaVarIndicee;
 import sa.SaVarSimple;
 import ts.Ts;
 
-public class Sa2ts extends SaDepthFirstVisitor<Void>{
-	
+public class Sa2ts extends SaDepthFirstVisitor<Void> {
+
+	private final Ts tableGlobale;
+	private Ts tableLocale;
+
 	public Sa2ts(SaNode saRoot) {
-		// TODO Auto-generated constructor stub
+		tableGlobale = new Ts();
+		saRoot.accept(this);
 	}
 
 	public Ts getTableGlobale() {
-		// TODO Auto-generated method stub
-		return null;
+		return tableGlobale;
 	}
 
 	@Override
 	public Void visit(SaDecTab node) {
-		// TODO Auto-generated method stub
+		String identif = node.getNom();
+		if (tableLocale != null) {
+			throw new RuntimeException("On ne peut pas déclarer un tableau dans une fonction");
+		}
+		if(tableGlobale.containsVar(identif)) {
+			throw new RuntimeException("Le tableau " + identif + " a déjà été défini");
+		}
 		return super.visit(node);
 	}
 
 	@Override
 	public Void visit(SaDecFonc node) {
-		// TODO Auto-generated method stub
-		return super.visit(node);
+		String identif = node.getNom();
+		tableLocale = new Ts();
+		if (!tableGlobale.containsFonc(identif)) {
+			throw new RuntimeException("La fontion" + identif + " a déjà été définie");
+		}
+		tableGlobale.addFct(node.getNom(), node.getParametres().length(), tableLocale, node);
+		super.visit(node);
+
+		tableLocale = null;
+
+		return null;
 	}
 
 	@Override
 	public Void visit(SaDecVar node) {
-		// TODO Auto-generated method stub
+		String identif = node.getNom();
+		Ts table = tableLocale == null ? tableGlobale : tableLocale;
+		if (table.containsVar(identif)) {
+			throw new RuntimeException("La variable " + identif + " a déjà été définie");
+		}
+		table.addVar(node.getNom(), 1);
 		return super.visit(node);
 	}
 
 	@Override
 	public Void visit(SaVarSimple node) {
-		// TODO Auto-generated method stub
+		String identif = node.getNom();
+		if (!(tableLocale != null && tableLocale.containsVar(identif))) {
+			if (!tableGlobale.containsFonc(identif)) {
+				throw new RuntimeException("La variable simple " + node.getNom() + "n'a pas été définie");
+			}
+		}
 		return super.visit(node);
 	}
 
 	@Override
 	public Void visit(SaAppel node) {
-		// TODO Auto-generated method stub
-		return super.visit(node);
-	}
-
-	@Override
-	public Void visit(SaInstRetour node) {
-		// TODO Auto-generated method stub
+		if (!tableGlobale.containsFonc(node.getNom())) {
+			throw new RuntimeException("La fonction simple " + node.getNom() + "n'a pas été définie");
+		}
 		return super.visit(node);
 	}
 
 	@Override
 	public Void visit(SaVarIndicee node) {
-		// TODO Auto-generated method stub
+		if (!tableGlobale.containsVar(node.getNom())) {
+			throw new RuntimeException("La variable indicee " + node.getNom() + "n'a pas été définie");
+		}
 		return super.visit(node);
 	}
 }
