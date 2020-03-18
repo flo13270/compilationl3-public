a506b94b92a09ca8032fc89f5bf4ee6118e0d74f
renaud <renaud.rochas2013@gmail.com>
2020-03-18 15:07:34 +0100

    mis les method override dans c3a2nasm

--- src/C3a2nasm.java
+++ src/C3a2nasm.java
@@ -1,8 +1,9 @@
-import c3a.C3a;
+import c3a.*;
 import nasm.Nasm;
 import ts.Ts;
 
-public class C3a2nasm {
+//TODO remplacer le T par une nasmOperand i guess ?
+public class C3a2nasm<T> implements C3aVisitor<T>{
 
 	public C3a2nasm(C3a c3a, Ts table) {
 		// TODO Auto-generated constructor stub
@@ -13,4 +14,113 @@ public class C3a2nasm {
 		return null;
 	}
 
+	@Override
+	public T visit(C3aInstAdd inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstCall inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstFBegin inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInst inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstJumpIfLess inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstMult inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstRead inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstSub inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstAffect inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstDiv inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstFEnd inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstJumpIfEqual inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstJumpIfNotEqual inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstJump inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstParam inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstReturn inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aInstWrite inst) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aConstant oper) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aLabel oper) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aTemp oper) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aVar oper) {
+		return null;
+	}
+
+	@Override
+	public T visit(C3aFunction oper) {
+		return null;
+	}
 }
